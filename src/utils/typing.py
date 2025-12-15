b362770631864926a9faac04dc9cf441ab641be4
Cade Wolfaardt <wolfaardtcade@gmail.com>
2025-12-16 02:00:12 +0400

    Simplify read and write modes

--- src/utils/typing.py
+++ src/utils/typing.py
@@ -5,8 +5,8 @@ from pathlib import Path
 # Verbosity for classes, functions, methods, etc.
 type Verbosity = Literal[0, 1, 2]
 # Mode for opening documents
-type ReadMode = Literal["r", "read"]
-type WriteMode = Literal["w", "write", "wx", "overwrite"]
+type ReadMode = Literal["r"]
+type WriteMode = Literal["w", "x"]
 type OpenMode = Literal[ReadMode, WriteMode]
 # Type alias for file/folder paths
 type Address = Union[str, Path]
