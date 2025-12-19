7dd115da40f86bc00b8868e9366b35c1331f4e0e
Cade Wolfaardt <wolfaardtcade@gmail.com>
2025-12-20 00:09:08 +0400

    Add type aliases for interpolation script

--- src/pv_inverter_modeling/utils/typing.py
+++ src/pv_inverter_modeling/utils/typing.py
@@ -1,13 +1,17 @@
 # stdlib
 from typing import (
-    Literal, 
+    Literal,
+    List,
     Union, 
     Tuple, 
-    TypeAliasType, 
+    TypeAliasType,
+    TypedDict,
     get_args, 
-    overload
+    overload,
 )
 from pathlib import Path
+# projectlib
+from pv_inverter_modeling.data.schemas import Column, Metric
 
 # Verbosity for classes, functions, methods, etc.
 type Verbosity = Literal[0, 1, 2]
@@ -30,7 +34,51 @@ type TypeValues = Union[
 ]
 # Type for either infering empirically or using a defined value
 type InferedFloat = Union[Literal["infer"], float]
-
+# Mode for loss preprocessing.interpolation loss injection 
+type LossMode = Literal[
+    "random", 
+    "blocks", 
+    "markov", 
+    "tod", 
+    "periodic", 
+    "stuck"
+]
+# Keyword arguments typing for loss injection internal methods
+class LossKwargs(TypedDict, total=False):
+    p: float
+    n_blocks: int
+    mean_len: float
+    p_miss: float
+    p_recover: float
+    start_state: str
+    p_by_hour: List[float]
+    k: int
+    jitter: int
+    n_segments: int
+# Keyword arguments for losss injection external call
+class InjectKwargs(LossKwargs, total=False):
+    mode: LossMode
+# Available interpolation methods 
+type InterpMethod = Literal[
+    "linear", 
+    "time", 
+    "index", 
+    "values", 
+    "nearest",
+    "zero", 
+    "slinear", 
+    "quadratic", 
+    "cubic", 
+    "polynomial", 
+    "spline",
+    "pchip", 
+    "akima", 
+    "cubicspline", 
+    "from_derivatives",
+]
+type InterpMethods = Tuple[InterpMethod, ...]
+# Possible columns
+type Field = Union[Column, Metric]
 # Overloads for custom_get_args function
 @overload
 def custom_get_args(arg: TypeAliasType) -> tuple[str, ...]: ...
